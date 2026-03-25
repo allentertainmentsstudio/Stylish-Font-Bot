56a76a60e89277f0739f011b5fc50c3ae74309dc
anujofficial9719-eng <anujofficial9719@gmail.com>
2026-03-25 22:56:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 from typing import List
 
-API_ID = os.environ.get("API_ID", "")
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = os.environ.get("API_ID", "34724970")
+API_HASH = os.environ.get("API_HASH", "f240eae7c60e8e30c17203ab0e052f7e")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8707891981:AAGHOAc6F5lwnLf8R1zBfm2V9PSdelfm010")
 PICS = (os.environ.get("PICS", "https://i.ibb.co/MDssddJp/pic.jpg https://i.ibb.co/n8fQ2xcx/pic.jpg")).split()
-ADMIN = int(os.environ.get("ADMIN", ""))
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
-DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "")
+ADMIN = int(os.environ.get("ADMIN", "7892805795"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1003515041061"))
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://Anujedit:Anujedit@cluster0.7cs2nhd.mongodb.net/?appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Anujedit")
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNELS", "").split())) # Add Multiple channel ids
 AUTH_REQ_CHANNELS = list(map(int, os.environ.get("AUTH_REQ_CHANNELS", "").split())) # Add Multiple channel ids
